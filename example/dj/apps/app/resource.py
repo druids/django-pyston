bca8c5cd0bda309edbdac270598ee2b7e5b74aa6
Lubos Matl <matllubos@gmail.com>
2016-11-23 21:31:27 +0100

    Fixed pyston serializer, sub requested fields must be get from defined fieldsets in resource

--- example/dj/apps/app/resource.py
+++ example/dj/apps/app/resource.py
@@ -7,7 +7,7 @@ from .models import Issue, User
 
 class IssueResource(BaseModelResource):
     model = Issue
-    default_detailed_fields = ('id', 'created_at', '_obj_name', 'name', ('created_by', ('contract',)), 'solver',
+    default_detailed_fields = ('id', 'created_at', '_obj_name', 'name', ('created_by', ('id', 'contract',)), 'solver',
                                'leader', 'watched_by')
     default_general_fields = ('id', '_obj_name', 'name', 'created_by', 'watched_by')
 
